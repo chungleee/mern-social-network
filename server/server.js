7d58aeebe8fc950ae18f1f9237fc9505c6b192bf
Leon <leon.karho.chung@gmail.com>
2018-07-09 10:23:27 -0400

    add config/key to ignore, basic mongoDB connection setup

--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const mongoose = require("mongoose")
 
 const app = express()
 
+// DB config
+const db = require('./config/keys').mongoURI
+
+// Connect to mongoDB
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch(err => console.log(err))
+
 app.get('/', (req, res) => {
-  res.send('hello')
+  res.send('hello world')
 })
 
 const port = process.env.PORT || 8080
