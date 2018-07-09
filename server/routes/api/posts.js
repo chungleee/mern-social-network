8d2a8aff52d23d2f9a798527afc2592bdc41d29b
Leon <leon.karho.chung@gmail.com>
2018-07-09 10:41:11 -0400

    setup api route boilerplate and test routes

---
+++ server/routes/api/posts.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router();
+
+// @route   GET api/posts/test
+// @desc    Test posts route
+// @access  Public
+router.get('/test', (req, res) => {
+  res.json({ msg: 'posts test route successful'})
+})
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const mongoose = require("mongoose")
 
+// routes
+const users = require('./routes/api/users')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
+
 const app = express()
 
 // DB config
@@ -17,6 +22,11 @@ mongoose
 app.get('/', (req, res) => {
   res.send('hello world')
 })
+
+// use routes
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
 
 const port = process.env.PORT || 8080
 
