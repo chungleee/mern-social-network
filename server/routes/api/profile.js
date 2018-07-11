4a6896a520db7867baed5b4f79cac23966e264dc
Leon <leon.karho.chung@gmail.com>
2018-07-11 17:46:41 -0400

    experience and education field delete routes, user/profile delete routes

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -144,7 +144,7 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
 
   // skills - split into array
   if(typeof skills !== 'undefined') {
-    profileFields.skills = skills.split(',').trim()
+    profileFields.skills = skills.split(',')
   }
 
   // social
@@ -272,5 +272,61 @@ router.post('/education', passport.authenticate('jwt', {session:false}), (req, r
         })
     })
 })
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience to profile route
+// @access  Private
+router.delete('/experience/:exp_id', passport.authenticate('jwt', {session:false}), (req, res) => {
+  Profile
+    .findOne({user: req.user.id})
+    .then(profile => {
+      // get remove index
+      const removeIdx = profile.experience.map(item => {
+        return item.id
+      })
+      .indexOf(req.params.exp_id)
+
+      profile.experience.splice(removeIdx, 1)
+
+      profile.save().then(profile => res.json(profile))
+    })
+    .catch(err => res.status(404).json(err))
+})
+
+// @route   DELETE api/profile/education/:edu_id
+// @desc    Delete education to profile route
+// @access  Private
+router.delete('/education/:edu_id', passport.authenticate('jwt', {session:false}), (req, res) => {
+  Profile
+    .findOne({user: req.user.id})
+    .then(profile => {
+      // get remove index
+      const removeIdx = profile.education.map(item => {
+        return item.id
+      })
+      .indexOf(req.params.edu_id)
+
+      profile.education.splice(removeIdx, 1)
+
+      profile.save().then(profile => res.json(profile))
+    })
+    .catch(err => res.status(404).json(err))
+})
+
+// @route   DELETE api/profile
+// @desc    Delete user and profile
+// @access  Private
+router.delete('/', passport.authenticate('jwt', {session:false}), (req, res) => {
+  Profile
+    .findOneAndRemove({user: req.user.id})
+    .then(() => {
+      User
+        .findOneAndRemove({_id: req.user.id})
+        .then(() => {
+          res.json({ success: true})
+        })
+    })
+})
+
 
 module.exports = router
